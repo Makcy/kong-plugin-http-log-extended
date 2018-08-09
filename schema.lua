3ca9afefc6e9aac537b4c10f3b4f40e375c5e280
Makcy <644215038@qq.com>
2018-08-09 16:43:49 +0800

    update

--- schema.lua
+++ schema.lua
@@ -1,10 +1,8 @@
 return {
   fields = {
     http_endpoint = { required = true, type = "url" },
-    method = { default = "POST", enum = { "POST", "PUT", "PATCH"} },
     timeout = { default = 10000, type = "number" },
     keepalive = { default = 60000, type = "number" },
-    content_type = { default = "application/json", enum = { "application/json" } }
     -- log_request_body = { type = "boolean", default = false },
     -- log_response_body = { type = "boolean", default = false }
   }
